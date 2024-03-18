3bbb2ee32070b8044ca27eea7ac34b14ac8df2ab
Rajat-Uchiha <rajatraguvanshi3900@gmail.com>
2024-03-18 22:18:02 +0530

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1 className="text-center text-9xl">Hello TechScholars</h1>;
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div className="text-center text-3xl">Hi I'm Navbar</div>;
+};
+
+export default Navbar;
