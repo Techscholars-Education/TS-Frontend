82a216173233fc6d3455d3696fb92ffd2ad28125
Rajat-Uchiha <rajatraguvanshi3900@gmail.com>
2024-03-19 12:45:02 +0530

    Heading and navbar done

--- components/Home/Heading.jsx
+++ components/Home/Heading.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Inter } from "next/font/google";
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+});
 
 const Heading = () => {
   return (
-    <div className="bg-[##FFE01B]">
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque,
-        explicabo.
+    <div className="bg-[#FFE01B]">
+      <p
+        className={`${inter.className} text-center py-2 text-md tracking-widest uppercase font-base`}
+      >
+        Your unfair advantage - Accelerate your journey towards success!
       </p>
     </div>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,10 +15,10 @@ const Navbar = () => {
     <header className="py-2 bg-gray-100">
       <nav className=" w-5/6 mx-auto flex justify-evenly items-center">
         <div className="flex items-center">
-          <Image className="w-20 h-20" src={Logo}></Image>
-          <h2 className={`${poppins.className} text-xl`}>TECHSCHOLARS</h2>
+          <Image className="w-16 h-16" src={Logo}></Image>
+          <h2 className={`${poppins.className} text-lg`}>TECHSCHOLARS</h2>
         </div>
-        <ul className={`${poppins.className} flex gap-10  text-xl `}>
+        <ul className={`${poppins.className} flex gap-10  text-lg `}>
           <li>
             <Link href="/">Courses</Link>
           </li>
@@ -37,7 +37,7 @@ const Navbar = () => {
           <button className="border border-black/40 rounded-xl px-4 py-2 text-lg text-neutral-800 hover:-translate-y-2 duration-300 auth-btn">
             Login
           </button>
-          <button className=" border border-black/40 rounded-xl px-4 py-2 bg-yellow-500 text-lg text-neutral-800 hover:-translate-y-2 duration-300  auth-btn">
+          <button className=" border border-black/40 rounded-xl px-4 py-2 bg-yellow-400 text-lg text-neutral-800 hover:-translate-y-2  duration-300  auth-btn">
             Signup
           </button>
         </div>
