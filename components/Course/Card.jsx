c87905f498b6ad643023e0ce6651c4ef9c53bb69
Rajat-Uchiha <rajatraguvanshi3900@gmail.com>
2024-04-14 00:04:07 +0530

    fixed conflicts

--- components/Course/Card.jsx
+++ components/Course/Card.jsx
@@ -1,31 +1,41 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-import Avatar from '@mui/material/Avatar';
-import AvatarGroup from '@mui/material/AvatarGroup';
-import Logo from '../../public/Logo.svg'
-import JEE from '../../public/Course/JEE.jpg'
+import Avatar from "@mui/material/Avatar";
+import AvatarGroup from "@mui/material/AvatarGroup";
+import JEE from "../../public/Course/JEE.jpg";
 
 const Card = () => {
   return (
     <>
       <div className="flex flex-col rounded-xl bg-slate-100 w-[296px] h-[428px] mx-6 font-Poppins">
         <div>
-          <Image
-            className="w-full rounded-xl "
-            src={JEE}
-          />
+          <Image className="w-full rounded-xl " src={JEE} />
         </div>
         <div className="flex justify-center">
           <div className="rounded-full w-4/6 py-1 relative bottom-6 flex justify-center bg-slate-200">
             <AvatarGroup
-              renderSurplus={(surplus) => <span>+{surplus.toString()[0]}k</span>}
+              renderSurplus={(surplus) => (
+                <span>+{surplus.toString()[0]}k</span>
+              )}
               total={4251}
             >
-              <Avatar alt="Remy Sharp" src={require("../../app/assets/course.png")} />
-              <Avatar alt="Travis Howard" src={require("../../app/assets/course.png")} />
-              <Avatar alt="Agnes Walker" src={require("../../app/assets/course.png")} />
-              <Avatar alt="Trevor Henderson" src={require("../../app/assets/course.png")} />
+              <Avatar
+                alt="Remy Sharp"
+                src={require("../../app/assets/course.png")}
+              />
+              <Avatar
+                alt="Travis Howard"
+                src={require("../../app/assets/course.png")}
+              />
+              <Avatar
+                alt="Agnes Walker"
+                src={require("../../app/assets/course.png")}
+              />
+              <Avatar
+                alt="Trevor Henderson"
+                src={require("../../app/assets/course.png")}
+              />
             </AvatarGroup>
           </div>
         </div>
@@ -33,16 +43,13 @@ const Card = () => {
         <div className="flex flex-col m-4">
           <div className="mb-4">
             <p className="font-base text-xs  ">1-28 july 2022</p>
-            <h3 className="font-semibold text-lg text-darkBlue ">
-              JEE
-            </h3>
+            <h3 className="font-semibold text-lg text-darkBlue ">JEE</h3>
             <span className="font-base text-xs text-slate-800">
               Product Management Masterclass, you will learn with Sarah Johnson
               - Head of Product Customer Platform Gojek Indonesia.
             </span>
           </div>
           <div className="flex justify-between mt-2">
-            
             <button className="bg-darkBlue  p-2 px-4 rounded-md text-sm text-white">
               <a href="/payment">Explore</a>
             </button>
--- components/Course/Course.jsx
+++ components/Course/Course.jsx
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 import React from "react";
 import Card from "./Card";
-=======
-import React from 'react'
-import Card from './Card'
-import CourseList from './CourseList'
->>>>>>> 598659c0c863ddf740e49b2757e8f820d25c895b
 
 const Course = () => {
   return (
     <>
-<<<<<<< HEAD
       <div className="bg-slate-100 font-Poppins">
         <div className="flex justify-center mt-8">
           <button className="bg-darkBlue p-2 px-4 text-white rounded-md">
@@ -29,31 +22,6 @@ const Course = () => {
             Select your package
           </span>
         </div>
-=======
-    <div className='bg-slate-100'>
-       
-       <div className='flex justify-center items-center my-8 flex-col'>
-                <h1 className='font-[600] text-[44px] text-[#002657] '>
-                    Welcome Harsh
-                </h1>
-                <span className='font-[500] text-[20px] text-[#002657] '>Select your package</span>
-            </div>
-        
-        <div className='flex justify-center w-[100vw] mt-4 '>
-            <Card/>
-            <Card/>
-            <Card/>
-           
-        </div>
-        <div className='flex justify-center mt-24 '>
-            <button className='bg-[#002657] text-white w-[200px] h-[48px] rounded-[10px] mx-8'>Back</button>
-            <button className='w-[200px] h-[52px] border-2  border-[#DCDCDC] rounded-[10px]'>Back to home</button>
-        </div>
-     </div>
-    </>
-  )
-}
->>>>>>> 598659c0c863ddf740e49b2757e8f820d25c895b
 
         <div className="flex justify-center w-[100vw] mt-4 ">
           <Card />
