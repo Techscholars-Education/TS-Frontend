6eb6657fda1564016ecad5ba217007c5644917d8
Ankit Raj <ankit.raj@lybl.com>
2024-05-02 23:17:43 +0530

    solve1

--- components/LeftNavigation/LeftNavigation.jsx
+++ components/LeftNavigation/LeftNavigation.jsx
@@ -1,7 +1,5 @@
-'use client'
 import React from "react";
 import Image from "next/image";
-import { usePathname} from "next/navigation";
 import {
   FiHome,
   FiBookOpen,
@@ -13,66 +11,93 @@ import {
   FiLogOut,
 } from "react-icons/fi";
 import Logo from "../../public/Logo.svg";
-import Link from "next/link";
-
 const LeftNavigation = () => {
-
-  const pathname = usePathname()
-
   return (
-    <nav className="bg-white w-[270px] py-8 px-4 flex flex-col justify-between items-center">
-      <div className="mx-auto fixed">
+    <nav className=" bg-slate-50   w-[16%] py-8 px-4 flex flex-col justify-between items-center">
+      <div className="mx-auto fixed  ">
         {/* Ist */}
         <div className="flex items-center  justify-center space-x-2 ">
           <Image
-            className="w-12 h-12"
+            className="w-10 h-10"
             src={Logo}
             alt="Techscholars-Logo"
           ></Image>
           <h1 className="text-gray-800 font-semibold ">TECHSCHOLARS</h1>
-        <div className="flex items-center justify-center space-x-2">
-          <Image className="w-10 h-10" src={Logo} alt="Techscholars-Logo" />
-          <h1 className="text-gray-800 font-semibold">TECHSCHOLARS</h1>
         </div>
+
+        {/* IInd */}
         <ul className="mt-20 mx-auto font-Poppins">
-          <li className={`flex items-center mb-10 ${pathname === "/home" ? 'text-[#002656]  py-2 bg-slate-100  ':'text-[#8A8A8A]'}`} >
+          <li className="flex items-center mb-10">
             <FiHome className="mr-2 text-xl" />
-            <a href="/home" className="font-semibold hover:text-gray-400">Home</a>
+            <a
+              href="#"
+              className="text-gray-500 font-semibold hover:text-gray-400"
+            >
+              Home
+            </a>
           </li>
-          <li className={`flex items-center mb-10 ${pathname === "/my-course"  ? 'text-[#002656] py-2 bg-slate-100':'text-[#8A8A8A]'}`}>
+          <li className="flex items-center mb-10">
             <FiBookOpen className="mr-2 text-xl" />
-            <a href="#" className="font-semibold hover:text-gray-400">My Course</a>
+            <a
+              href="#"
+              className="text-gray-500 font-semibold  hover:text-gray-400"
+            >
+              My Course
+            </a>
           </li>
-          <li className={`flex items-center mb-10 ${pathname === "/test-series"  ? 'text-[#002656] py-2 bg-slate-100':'text-[#8A8A8A]'}`}>
+          <li className="flex items-center mb-10">
             <FiClipboard className="mr-2 text-xl" />
-            <a href="test-series" className="font-semibold hover:text-gray-400">Test Series</a>
+            <a
+              href="#"
+              className="text-gray-500 font-semibold  hover:text-gray-400"
+            >
+              Test Series
+            </a>
           </li>
-          <li className={`flex items-center mb-10 ${pathname === "/referral"  ? 'text-[#002656] py-2 bg-slate-100':'text-[#8A8A8A]'}`}>
+          <li className="flex items-center mb-10">
             <FiUsers className="mr-2 text-xl" />
-            <a href="/referral" className="font-semibold hover:text-gray-400">Refer and Earn</a>
+            <a
+              href="#"
+              className="text-gray-500 font-semibold  hover:text-gray-400"
+            >
+              Refer and Earn
+            </a>
           </li>
-          <li className={`flex items-center mb-10 ${pathname=== "/feed"  ? 'text-[#002656] py-2 bg-slate-100':'text-[#8A8A8A]'}`}>
+          <li className="flex items-center mb-10">
             <FiRss className="mr-2 text-xl" />
-            <a href="#" className="font-semibold hover:text-gray-400">Feed</a>
+            <a
+              href="#"
+              className="text-gray-500 font-semibold hover:text-gray-400"
+            >
+              Feed
+            </a>
           </li>
-          <li className={`flex items-center mt-10 ${pathname=== "/community"  ? 'text-[#002656] py-2 bg-slate-100':'text-[#8A8A8A]'}`}>
+          <li className="flex items-center mt-10">
             <FiMessageCircle className="mr-2 text-xl" />
-            <a href="/community" className="font-semibold hover:text-gray-400">Community</a>
+            <a
+              href="#"
+              className="text-gray-500 font-semibold hover:text-gray-400"
+            >
+              Community
+            </a>
           </li>
-          <li className={`flex items-center mt-10 ${pathname === "/settings"  ? 'text-[#002656] py-2 bg-slate-100':'text-[#8A8A8A]'}`}>
+          <li className="flex items-center mt-10">
             <FiSettings className="mr-2 text-xl" />
-            <a href="/settings" className="font-semibold hover:text-gray-400">Settings</a>
+            <a
+              href="#"
+              className="text-gray-500 font-semibold  hover:text-gray-400"
+            >
+              Settings
+            </a>
           </li>
         </ul>
+
+        {/* 3rd */}
       </div>
       <div>
-        <button className="text-white ml-8 hover:text-gray-400 flex items-center ">
-        <button className="text-white absolute bottom-6 left-12 hover:text-gray-400 flex items-center ">
-          <Link href="/" className="flex">
+        <button className="text-white absolute bottom-6 left-12  hover:text-gray-400 flex items-center ">
           <FiLogOut className="mr-2 text-[#E55858] text-xl" />
-          
           <span className="text-[#E55858] font-bold ">Sign Out</span>
-          </Link>
         </button>
       </div>
     </nav>
