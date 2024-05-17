b514fc5fc8e55229c162e7e8944f2c2d560cc936
Satyam750 <satyamthakur05@gmail.com>
2024-05-17 11:55:10 +0530

    admin-dashboard

---
+++ app/(admin-dashboard)/admin/notifications/page.jsx
@@ -0,0 +1,98 @@
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import Nav from '../../_components/Nav';
+import AdminSidebar from '../../_components/AdminSidebar';
+
+
+const page = () => {
+    const [domLoaded, setDomLoaded] = useState(false);
+
+    // const [selectedValue, setSelectedValue] = useState('');
+
+    // const handleChange = (value) => {
+
+    //     setSelectedValue(value)
+    //     console.log(value);
+    // }
+
+    useEffect(() => {
+        setDomLoaded(true);
+    }, []);
+
+    return (
+        <>
+            {
+                domLoaded ? (
+                    <div className='h-screen w-full '>
+                        <Nav />
+                        <div>
+                            <div><AdminSidebar /></div>
+                        </div>
+                        <div id='batches' className='bg-gray-100 overflow-y-auto h-[41vw] w-[85vw] relative right-[-15vw]  rounded-md'>
+                            <div>
+                                <h1 className='font-semibold text-xl ml-10 mt-2'>Notification</h1>
+                            </div>
+
+                            <div className='h-[18vw] w-[80vw] flex ml-10 mt-3 '>
+                                <div className='bg-white  w-[50vw] pl-3 flex flex-col gap-5 pt-5  rounded-l-md'>
+                                    <h1 className='font-semibold text-[.9vw]'>Heading</h1>
+                                    <p className='font-semibold text-[.85vw]'>Body</p>
+                                </div>
+                                <div className='w-[30vw] bg-[#002956] text-white rounded-md flex flex-col gap-[3vw] pl-[5vw]'>
+                                    <div id='option' className=' flex flex-col gap-4 mt-4'>
+                                        <div className='flex items-center gap-[8vw]'>
+                                            <h1 className='font-semibold text-[.8vw]'>Class</h1>
+                                            <div className="w-30">
+                                                <select id="countries" defaultValue={"9th"} className="bg-[#002956] text-[.9vw] cursor-pointer text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[6vw] p-2.5 ">
+                                                    <option  value="9th">9th</option>
+                                                    <option value="10th">10th</option>
+                                                    <option value="11th">11th</option>
+                                                    <option value="12th">12th</option>
+                                                </select>
+
+                                            </div>
+                                        </div>
+                                        <div className='flex items-center gap-[8vw]'>
+                                            <h1 className='font-semibold text-[.8vw]'>Types</h1>
+                                            <div className="w-30">
+                                            <select defaultValue={"foundation"} id="countries" className="bg-[#002956] cursor-pointer text-[.9vw] text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[6vw] p-2.5 ">
+                                                    <option value="foundation">Foundation</option>
+                                                    <option value="ceo">CEO</option>
+                                                    <option value="cto">CTO</option>
+                                                    <option value="manager">Manager</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className='flex items-center gap-[8vw]'>
+                                            <h1 className='font-semibold text-[.8vw]'>Plan</h1>
+                                            <div className="w-30 ml-2">
+                                            <select defaultValue={"starter"} id="countries" className="bg-[#002956] cursor-pointer text-[.9vw]  text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[6vw] p-2.5 ">
+                                                    <option value="starter">Starter</option>
+                                                    <option value="basic">Basic</option>
+                                                    <option value="highest">Highest</option>
+                                                    <option value="premium">Premium</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='relative'>
+                                        <button className='absolute right-3 pl-3 pr-3 pt-1 pb-1 h-10 w-[8vw] bg-yellow-400 rounded-xl text-black hover:bg-yellow-700 hover:text-white transition-all ease-in-out duration-200'>Send</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                ) : (
+                    <div className='flex items-center justify-center h-screen w-full'>
+                        <div className="loader"></div>
+                    </div>
+
+                )
+            }
+        </>
+    )
+}
+
+export default page
---
+++ app/(admin-dashboard)/admin/scholorship/_components/Quiz.jsx
@@ -0,0 +1,130 @@
+"use client";
+
+import { useState } from "react";
+import { quiz } from "./data";
+import "./quiz.css"
+
+const Quiz = () => {
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [showResult, setShowResult] = useState(false);
+  const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
+  const [result, setResult] = useState({
+    score: 0,
+    correctAnswers: 0,
+    wrongAnswers: 0,
+  });
+
+  const { questions } = quiz;
+  const { question, choices, correctAnswer } = questions[activeQuestion];
+
+  const onClickNext = () => {
+    setSelectedAnswerIndex(null);
+    setResult((prev) =>
+      selectedAnswer
+        ? {
+            ...prev,
+            score: prev.score + 5,
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : { ...prev, wrongAnswers: prev.wrongAnswers + 1 }
+    );
+    if (activeQuestion !== questions.length - 1) {
+      setActiveQuestion((prev) => prev + 1);
+    } else {
+      setActiveQuestion(0);
+      setShowResult(true);
+    }
+  };
+
+  const onAnswerSelected = (answer, index) => {
+    setSelectedAnswerIndex(index);
+    if (answer === correctAnswer) {
+      setSelectedAnswer(true);
+    } else {
+      setSelectedAnswer(false);
+    }
+  };
+
+  const addLeadingZero = (number) => (number > 9 ? number : `0${number}`);
+
+  const onDelete = () =>{
+    setSelectedAnswerIndex(null);
+  }
+
+  return (
+    <div className="quiz-container">
+      {!showResult ? (
+        <div>
+          <span className="border-2 border-gray-300 rounded-lg p-2 ">
+            <span className="text-[.8vw] font-semibold font-Poppins ">
+              {addLeadingZero(activeQuestion + 1)}
+            </span>
+            <span className="text-[.8vw] font-semibold font-Poppins">
+              /{addLeadingZero(questions.length)}
+            </span>
+          </span>
+          <div className="bg-[#B7AFE7] rounded-lg w-[33vw] mt-5 h-[7vw] p-3">
+            <h1 className="font-semibold text-gray-300">{question}</h1>
+          </div>
+           
+          <div className="flex gap-5 text-[.8vw] mt-2 mb-2">
+            <button className="font-semibold">MCQ</button>
+            <button className="font-light">Numerical</button>
+            <button className="font-light">MSQ</button>
+          </div>
+          <ul className=" w-[14vw]">
+            {choices.map((answer, index) => (
+              <li
+                onClick={() => onAnswerSelected(answer, index)}
+                key={answer}
+                className={
+                  `${selectedAnswerIndex === index ? "selected-answer" : null}`
+                  
+                }
+              >
+                {answer}
+              </li>
+            ))}
+          </ul>
+          <div className="flex gap-4 mt-5">
+            <button
+            className="border-gray-300 rounded-lg border-2 p-2 hover:border-pink-300 hover:text-pink-300 transition-all ease-in-out duration-200"
+              onClick={onDelete}
+              disabled={selectedAnswerIndex === null}
+            >
+              {activeQuestion === questions.length - 1 ? "Finish" : "Delete"}
+            </button>
+            <button
+             className="border-gray-300 rounded-lg border-2 p-2 hover:border-green-200 hover:text-green-200 transition-all ease-in-out duration-200"
+              onClick={onClickNext}
+              disabled={selectedAnswerIndex === null}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      ):(
+        <div className="result">
+          <h3>Result</h3>
+          <p>
+            Total Question: <span>{questions.length}</span>
+          </p>
+          <p>
+            Total Score:<span> {result.score}</span>
+          </p>
+          <p>
+            Correct Answers:<span> {result.correctAnswers}</span>
+          </p>
+          <p>
+            Wrong Answers:<span> {result.wrongAnswers}</span>
+          </p>
+        </div>
+      )
+
+      }
+    </div>
+  );
+};
+
+export default Quiz;
