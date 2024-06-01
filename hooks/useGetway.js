623a64cb35c5340de2f433a5524304c5db20cb0c
Satyam750 <satyamthakur05@gmail.com>
2024-06-01 21:45:58 +0530

    added dashboard feed

--- hooks/useGetway.js
+++ hooks/useGetway.js
@@ -1,18 +1,22 @@
 "use client";
-import Cookies from "js-cookie";
-import { useState } from "react";
+
 import { toast } from "react-toastify";
 
 const useGetway = () => {
 
-  const [checking, setChecking] = useState(false);
-  const getway = async (pdt_type,pdt_id,ts_id,uid,discount_code) => {
+  const getway = async () => {
     try {
-      const res = await fetch("https://api.techscholars.in/order/subscriptions", {
+      const res = await fetch("https://api.techscholars.co.in/order/subscriptions", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({pdt_type,pdt_id,ts_id,uid,discount_code }),
-        mode: 'no-cors',
+        body: JSON.stringify({
+          pdt_type: "1",
+          pdt_id: 1,
+          ts_id: null,
+          uid: 1,
+          discount_code: null
+        }),
+        mode: 'no-cors'
       });
       const data = await res.json();
       console.log(data);
@@ -21,7 +25,7 @@ const useGetway = () => {
         throw new Error(data.error);
       }
 
-     
+
     } catch (error) {
       toast.error(error.message);
     }
