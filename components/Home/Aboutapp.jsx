54bbc595d6da4a15061172896b8a657f756efd46
Rajat-Uchiha <rajatraguvanshi3900@gmail.com>
2024-06-05 00:44:00 +0530

    working on about app section/ youtube videos section

--- components/Home/Aboutapp.jsx
+++ components/Home/Aboutapp.jsx
@@ -1,39 +1,28 @@
 import React from "react";
 import Image from "next/image";
-import Phone from "../../public/Home/Phone.png";
-import playstore from "../../public/Home/playstore.svg";
-import applestore from "../../public/Home/applestore.svg";
+import mobile from "../../public/Home/mobile.png";
 
 const Aboutapp = () => {
   return (
-    <section className=" flex flex-col-reverse md:flex md:flex-row  w-11/12 mx-auto font-Poppins md:space-x-10 bg-gray-50 ">
-      <div className=" w-full md:w-1/2 flex justify-center  ">
-        <Image src={Phone} alt="Techscholar-app-image"></Image>
-      </div>
-      <div className="w-full md:w-1/2 flex flex-col justify-center items-center space-y-4 md:pl-10 ">
-        <div>
-          <h2 className=" text-center md:text-start text-3xl lg:text-3xl xl:text-5xl font-semibold text-darkBlue xl:leading-normal">
-            Start achieving your acadamic excellence with Techscholars.
-          </h2>
-        </div>
-        <div className="w-full space-x-4 flex justify-center items-center  md:block ">
-          <button>
-            <Image
-              className=" w-32 md:w-20 lg:w-32 xl:w-40"
-              src={playstore}
-              alt="play-store-icon"
-            ></Image>
-          </button>
-          <button>
-            <Image
-              className=" w-32 md:w-20 lg:w-32 xl:w-40"
-              src={applestore}
-              alt="app-store-icon"
-            ></Image>
-          </button>
+    <>
+      <div className=" w-full mx-auto  rounded-xl py-6">
+        <h2 className="text-darkBlue font-semibold text-xl  md:text-4xl px-10 md:px-0 text-center my-4 font-Poppins ">
+          Join Bharat&#8216;s Most Trusted Educational Platform Today
+        </h2>
+        <div className="my-4 text-center  ">
+          <p className="font-Poppins px-6 leading-normal  md:px-20 lg:px-44 text-xs md:text-sm text-gray-600 tracking-wide ">
+            Explore our YouTube Channel and subscribe to get access to quality
+            education for free.
+          </p>
         </div>
       </div>
-    </section>
+      <section className=" flex flex-col-reverse md:flex md:flex-row  w-11/12 mx-auto font-Poppins md:space-x-10 ">
+        <div className=" w-full md:w-1/3 flex justify-center  ">
+          <Image src={mobile} alt="Techscholar-app-image"></Image>
+        </div>
+        <div className=" grid grid-cols-2 w-full gap-6 "></div>
+      </section>
+    </>
   );
 };
 
