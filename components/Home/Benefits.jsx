7cc7e99970743153966ac6e49c4ce8e4752b2a48
Rajat-Uchiha <rajatraguvanshi3900@gmail.com>
2024-06-04 20:53:49 +0530

    worked on benefits page -> updated size configurations

--- components/Home/Benefits.jsx
+++ components/Home/Benefits.jsx
@@ -9,14 +9,14 @@ import login from "../../public/Home/login.png";
 const Benefits = () => {
   return (
     <section className="w-full bg-gradient-to-r from-TechBlue/5 to-white py-10  ">
-      <div className=" w-11/12 xl:w-9/12 grid grid-cols-1 md:grid-cols-2  mx-auto gap-4 ">
+      <div className=" w-11/12 xl:w-10/12 grid grid-cols-1 md:grid-cols-2  mx-auto gap-4 ">
         {/* container -1 */}
-        <div className=" bg-[#DCD9F866] rounded-xl  flex px-6 py-6 md:py-12 font-Poppins shadow-md hover:shadow-xl duration-200 transition-all ">
-          <div className="my-auto">
+        <div className=" bg-[#DCD9F866]/30 rounded-xl flex px-6 py-6 lg:py-8 xl:py-12 font-Poppins shadow-md hover:shadow-xl duration-200 transition-all ">
+          <div className="my-auto w-1/2">
             <Image className=" w-full " src={ratings} alt="rating-icon"></Image>
           </div>
-          <div className="w-full px-4 lg:px-10   ">
-            <h3 className="text-darkBlue font-semibold mb-1 ">
+          <div className="w-full px-4 lg:px-10  ">
+            <h3 className="text-darkBlue font-semibold mb-1 text-sm md:text-base lg:text-xl ">
               High quality Content
             </h3>
             <p className="text-xs md:text-sm text-gray-600">
@@ -28,17 +28,17 @@ const Benefits = () => {
             </button>
           </div>
         </div>
-        <div className=" bg-[#FFE6DF66] rounded-xl flex px-6 py-6 md:py-12 font-Poppins shadow-md hover:shadow-xl duration-200 transition-all ">
-          <div className="my-auto">
+        <div className=" bg-[#FFE6DF]/30 rounded-xl flex px-6 py-6 lg:py-8 xl:py-12 font-Poppins shadow-md hover:shadow-xl duration-200 transition-all ">
+          <div className="my-auto w-1/2">
             <Image
               className=" w-full "
               src={schdule_time}
-              alt="schedule-time"
+              alt="timing-icon"
             ></Image>
           </div>
           <div className="w-full px-4 lg:px-10   ">
-            <h3 className="text-darkBlue font-semibold mb-1 ">
-              Doubt Solving Classes
+            <h3 className="text-darkBlue font-semibold mb-1 text-sm md:text-base lg:text-xl ">
+              Doubt solving classes
             </h3>
             <p className="text-xs md:text-sm text-gray-600">
               Unlock clarity in our Doubt Solving Classes: dedicated sessions
@@ -49,16 +49,18 @@ const Benefits = () => {
             </button>
           </div>
         </div>
-        <div className=" bg-[#FCE2EB4D] rounded-xl flex px-6 py-6 md:py-12 font-Poppins shadow-md hover:shadow-xl duration-200 transition-all ">
-          <div className="my-auto">
+        <div className=" bg-[#FCE2EB]/30 rounded-xl flex px-6 py-6 lg:py-8 xl:py-12 font-Poppins shadow-md hover:shadow-xl duration-200 transition-all ">
+          <div className="my-auto w-1/2">
             <Image
               className=" w-full "
               src={live_class}
-              alt="live-class"
+              alt="liveclass-icon"
             ></Image>
           </div>
           <div className="w-full px-4 lg:px-10   ">
-            <h3 className="text-darkBlue font-semibold mb-1 ">Weekly tests</h3>
+            <h3 className="text-darkBlue font-semibold mb-1 text-sm md:text-base lg:text-xl ">
+              Weekly tests
+            </h3>
             <p className="text-xs md:text-sm text-gray-600">
               Embark on a journey of assessment excellence with our All India
               test series
@@ -68,12 +70,14 @@ const Benefits = () => {
             </button>
           </div>
         </div>
-        <div className=" bg-[#DCF4F566] rounded-xl flex px-6 py-6 md:py-12 font-Poppins shadow-md hover:shadow-xl duration-200 transition-all ">
-          <div className="my-auto">
-            <Image className=" w-full " src={login} alt="login-icon"></Image>
+        <div className=" bg-green-50 rounded-xl flex px-6 py-6 lg:py-8 xl:py-12 font-Poppins shadow-md hover:shadow-xl duration-200 transition-all ">
+          <div className="my-auto w-1/2">
+            <Image className=" w-full " src={login} alt="rating-icon"></Image>
           </div>
           <div className="w-full px-4 lg:px-10   ">
-            <h3 className="text-darkBlue font-semibold mb-1 ">DPPs</h3>
+            <h3 className="text-darkBlue font-semibold mb-1 text-sm md:text-base lg:text-xl ">
+              DPPs
+            </h3>
             <p className="text-xs md:text-sm text-gray-600">
               Dive into enriched learning with our meticulously curated DPP and
               Study Material
