f060cef82cb98951c47de6bae0aa30cade19c7ab
Rajat-Uchiha <rajatraguvanshi3900@gmail.com>
2024-06-09 12:53:24 +0530

    added arrows props to false in react slick slider

--- components/Home/Faculties.jsx
+++ components/Home/Faculties.jsx
@@ -13,6 +13,7 @@ const Faculties = (props) => {
     slidesToShow: 3,
     autoplaySpeed: 2000,
     slidesToScroll: 1,
+    arrows: false,
     initialSlide: 0,
     rtl: true,
     autoplay: true,
@@ -20,7 +21,7 @@ const Faculties = (props) => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
         },
