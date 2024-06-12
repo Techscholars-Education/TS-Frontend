85aaaf7abc8a6b5073b09288a375cd18f92b3f6f
Rajat-Uchiha <rajatraguvanshi3900@gmail.com>
2024-06-12 12:52:22 +0530

    footer added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { toast, ToastContainer } from "react-toastify";
