366833d4b94d06db84646549e664373cfa60b0c3
Rajat-Uchiha <rajatraguvanshi3900@gmail.com>
2024-06-24 18:37:19 +0530

    added landing loader

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -1,3 +1,6 @@
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
 import Hero from "@/components/Home/Hero";
 import Benefits from "@/components/Home/Benefits";
 import Curriculum from "@/components/Home/Curriculum";
@@ -10,21 +13,42 @@ import Navbar from "@/components/Navbar";
 import Testimonialsslider from "@/components/Home/Testimonialsslider";
 import Faculties from "./Faculties";
 import Price from "./Price";
+import sciencebook from "@/public/sciencebook.gif";
 const Home = () => {
+  const [isLoaded, setIsLoaded] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(false);
+    }, 2000);
+  }, []);
+
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Benefits />
-      <Curriculum />
-      <About />
-      <Price />
-      <Faculties calling={"home"} />
-      <Testimonials />
-      <Testimonialsslider />
-      <Studenttest />
-      <Aboutapp />
-      <Footer />
+      {isLoaded ? (
+        <div className="h-screen w-full flex items-center justify-center">
+          <Image
+            src={sciencebook}
+            alt="science-book-loader"
+            className="lg:h-[20vh] lg:w-[10vw] h-[20vh] w-[35vw]  "
+          />
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Hero />
+          <Benefits />
+          <Curriculum />
+          <About />
+          <Price />
+          <Faculties calling={"home"} />
+          <Testimonials />
+          <Testimonialsslider />
+          <Studenttest />
+          <Aboutapp />
+          <Footer />
+        </>
+      )}
     </>
   );
 };
