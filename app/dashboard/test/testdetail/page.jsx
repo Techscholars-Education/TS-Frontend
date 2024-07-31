7e8fe11e1ab0df803958c6b1dda9565bdec8096c
Rajat Kalotra <rajatraguvanshi3900@gmail.com>
2024-07-31 23:54:48 +0530

    updated test-series cards

--- app/dashboard/test/testdetail/page.jsx
+++ app/dashboard/test/testdetail/page.jsx
@@ -1,20 +1,19 @@
-import DashboardNavbar from '@/components/Dashboard/DashboardNavbar'
-import TestDetail from '@/components/Dashboard/Test/TestDetail'
-import LeftNavigation from '@/components/LeftNavigation/LeftNavigation'
-import React from 'react'
+import DashboardNavbar from "@/components/Dashboard/DashboardNavbar";
+import TestDetail from "@/components/Dashboard/Test/TestDetail";
+import LeftNavigation from "@/components/LeftNavigation/LeftNavigation";
+import React from "react";
 
 function page() {
   return (
     <div>
-            <section className="flex w-full overflow-x-hidden">
-                <LeftNavigation/>
-                <div className='flex md:flex-col'>
-
-                  
-                    <TestDetail/>
-                </div>
-            </section></div>
-  )
+      <section className="flex w-full overflow-x-hidden">
+        <LeftNavigation />
+        <div className="flex md:flex-col">
+          <TestDetail />
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default page
+export default page;
--- components/Dashboard/Test/TestSeriesCard.jsx
+++ components/Dashboard/Test/TestSeriesCard.jsx
@@ -1,61 +1,55 @@
-import React from 'react'
+import React from "react";
 import Link from "next/link";
 import star from "../../../public/Home/star.svg";
 import { FaClock } from "react-icons/fa6";
 import { PiGraduationCapFill } from "react-icons/pi";
 import { FaFire } from "react-icons/fa6";
-import Image from 'next/image';
+import Image from "next/image";
 import avatar from "../../../public/Dashboard/avatar.jpg";
 
 function TestSeriesCard(props) {
+  const item = { ...props.item };
   return (
     <div
-      className={`flex flex-col rounded-xl border md:w-[20vw]  font-Poppins relative bg-white`}
+      className={`flex flex-col rounded-xl font-Poppins relative bg-white border  `}
     >
-      <div>
-        <Image
+      <div className="h-52 w-full bg-gray-300 animate-pulse rounded-xl flex justify-center items-center  ">
+        {/* <Image
           className="w-full rounded-xl"
           alt="course-posters"
           src={props.image}
-          style={{ height: '200px', width: '400px' }}
-          width={400}  
+          style={{ height: "200px", width: "400px" }}
+          width={400}
           height={200}
-        />
+        /> */}
+        <div className="h-10 w-10 border-2 border-gray-400 animate-spin rounded-xl"></div>
       </div>
-
-
-      <div className="flex flex-col m-4 my-4">
+      <div className="p-4 font-Poppins text-darkBlue space-y-2">
         <div>
-          <div className="flex  items-center">
-
-            <div className="flex flex-col ">
-              <div className='flex mb-2'>
-                 <Link href="#" className='bg-[#FF7512] rounded-full ml-0 mx-2 px-2 text-white text-sm py-1'>{props.std}th</Link>
-                 <Link href="#" className='bg-[#9F46E4] rounded-full mx-2 px-2 text-white text-sm py-1'>Foundation</Link>
-                 <Link href="#" className='bg-[#17DB7E] rounded-full mx-2 px-2 text-white text-sm py-1'>2024</Link>
-              </div>
-
-             <div className='h-9'>
-             <p className="font-semibold text-sm ">{props.name}</p>
-             </div>
-            </div>
-          </div>
+          <h4 className="font-semibold text-lg">{item.heading}</h4>
         </div>
-        <p className="font-base text-xs xl:text-sm leading-relaxed text-gray-500  font-Poppins">
-         {props.description}
-        </p>
-        {/* <p className='text-[16px] font-semibold'>
-          -By Md. Mahmodul Hasan
-        </p> */}
+        <div>
+          <p className="text-gray-500 text-sm">{item.description}</p>
+        </div>
+        <ul className="list-disc ml-4 text-sm font-medium text-gray-500 marker:text-TechBlue space-y-2 ">
+          {item.bulletPoints.map((point) => {
+            return <li>{point}</li>;
+          })}
+        </ul>
 
+        <hr />
 
-
-
-
-
+        <div className="w-full  ">
+          <Link
+            className="bg-TechBlue mt-4 text-white rounded-full w-full py-3 hover:bg-black duration-200 inline-block text-center font-medium font-Poppins"
+            href="#"
+          >
+            Get started
+          </Link>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default TestSeriesCard
+export default TestSeriesCard;
