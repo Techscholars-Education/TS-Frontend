c07e0eebeb9e5a6a129ee89c71f04e15d55e130f
Satyam750 <satyamthakur05@gmail.com>
2024-08-01 13:06:12 +0530

    add calender in home page

--- components/Dashboard/Calendar/Calenders.jsx
+++ components/Dashboard/Calendar/Calenders.jsx
@@ -6,36 +6,38 @@ import { DayPicker } from 'react-day-picker';
 import "react-day-picker/style.css";
 import cal from "./cal.css";
 
-const Calenders = ({ onRangeChange }) => {
-    const defaultMonth = new Date();
-  
-    const defaultSelected = {
-      from: defaultMonth,
-      to: addDays(defaultMonth, 4)
-    };
-  
-    const [range, setRange] = useState(defaultSelected);
-  
-    // Function to disable past dates
-    const disablePastDates = (date) => {
-      const today = startOfToday();
-      return isBefore(date, today);
-    };
-  
-    // Function to handle range selection
-    const handleSelect = (selectedRange) => {
-      setRange(selectedRange);
-      if (onRangeChange) {
-        onRangeChange(selectedRange);
-      }
-    };
 
-    // Notify parent component of the default range on mount
-    useEffect(() => {
-      if (onRangeChange) {
-        onRangeChange(range);
-      }
-    }, [onRangeChange, range]);
+
+const Calenders = ({ onRangeChange, initialRange }) => {
+  const defaultMonth = new Date();
+
+  const defaultSelected = {
+    from: defaultMonth,
+    to: addDays(defaultMonth, 4)
+  };
+
+  const [range, setRange] = useState(initialRange || defaultSelected);
+
+  useEffect(() => {
+    if (onRangeChange) {
+      onRangeChange(range);
+    }
+  }, [range, onRangeChange]);
+
+  // Notify parent component of the default range on mount
+  useEffect(() => {
+    if (initialRange) {
+      setRange(initialRange);
+    }
+  }, [initialRange]);
+
+
+        // Function to disable past dates
+        const disablePastDates = (date) => {
+          const today = startOfToday();
+          return isBefore(date, today);
+        };
+      
 
   return (
     <>
@@ -46,7 +48,7 @@ const Calenders = ({ onRangeChange }) => {
           mode="range"
           defaultMonth={defaultMonth}
           selected={range}
-          onSelect={handleSelect} // Handle selection change
+          onSelect={() => {}} // Disable user selection
           disabled={disablePastDates} // Disable past dates
         />
       </div>
