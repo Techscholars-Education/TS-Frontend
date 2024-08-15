fc490f25ca84bd9329463f0bf16da2134648916e
Satyam750 <satyamthakur05@gmail.com>
2024-08-15 22:57:46 +0530

    dynamic routes for courses

--- components/Dashboard/Course/Classes.jsx
+++ components/Dashboard/Course/Classes.jsx
@@ -1,14 +1,17 @@
+"use client"
 import React from 'react'
 import CourseDetailNavbar from './CourseDetailNavbar'
-import { FaSquareRootAlt, FaAtom, FaFlask, FaVial } from 'react-icons/fa';
-import s1 from "../../../public/Dashboard/Subject/s1.png"
-import s2 from "../../../public/Dashboard/Subject/s2.png"
-import s3 from "../../../public/Dashboard/Subject/s3.png"
-import s4 from "../../../public/Dashboard/Subject/s4.png"
-import s5 from "../../../public/Dashboard/Subject/s5.png"
+import s1 from "@/public/Dashboard/Subject/s1.png"
+import s2 from "@/public/Dashboard/Subject/s2.png"
+import s3 from "@/public/Dashboard/Subject/s3.png"
+import s4 from "@/public/Dashboard/Subject/s4.png"
+import s5 from "@/public/Dashboard/Subject/s5.png"
 import Link from 'next/link';
 import Image from 'next/image';
+import { useParams } from 'next/navigation';
 function Classes() {
+  const params = useParams()
+
     const subjects = [
         { id:1, subject: 'Notices', chapters: 16, icon: <div className='h-10 w-10 bg-[#FDFDFD] flex items-center justify-center rounded-md'><Image src={s1} alt='s1' className='h-7 w-7'/></div> },
         { id:2, subject: 'Maths', chapters: 21, icon: <div className='h-10 w-10 bg-[#FDFDFD] flex items-center justify-center rounded-md'><Image src={s2} alt='s2' className='h-7 w-7'/></div> },
@@ -21,7 +24,7 @@ function Classes() {
       
   return (
     <div className=" font-Poppins min-h-screen py-6 md:full md:mr-8 flex flex-col bg-[#f7faff] ">
-    <CourseDetailNavbar/>
+    <CourseDetailNavbar courseRoute={params.coursedetail} />
     <div className='flex flex-col  bg-white rounded-xl p-3 md:mr-10 mt-4 md:ml-10'>
         <div className='m-4'>
             <h2 className='text-[20px] font-[550]'>Subject</h2>
