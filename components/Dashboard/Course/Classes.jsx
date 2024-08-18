99c1978471510d51e56b2c35dd14f75fd769636a
Satyam750 <satyamthakur05@gmail.com>
2024-08-18 11:22:59 +0530

    fex lecture page

--- components/Dashboard/Course/Classes.jsx
+++ components/Dashboard/Course/Classes.jsx
@@ -1,21 +1,24 @@
-"use client";
-import React from "react";
-import CourseDetailNavbar from "./CourseDetailNavbar";
-import s1 from "@/public/Dashboard/Subject/s1.png";
-import s2 from "@/public/Dashboard/Subject/s2.png";
-import s3 from "@/public/Dashboard/Subject/s3.png";
-import s4 from "@/public/Dashboard/Subject/s4.png";
-import s5 from "@/public/Dashboard/Subject/s5.png";
-import Link from "next/link";
-import Image from "next/image";
-import { useParams } from "next/navigation";
+'use client';
+
+import React from 'react';
+import CourseDetailNavbar from './CourseDetailNavbar';
+import s1 from '@/public/Dashboard/Subject/s1.png';
+import s2 from '@/public/Dashboard/Subject/s2.png';
+import s3 from '@/public/Dashboard/Subject/s3.png';
+import s4 from '@/public/Dashboard/Subject/s4.png';
+import s5 from '@/public/Dashboard/Subject/s5.png';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useParams } from 'next/navigation';
+import { motion } from 'framer-motion';
+
 function Classes() {
   const params = useParams();
 
   const subjects = [
     {
       id: 1,
-      subject: "Notices",
+      subject: 'Notices',
       chapters: 16,
       icon: (
         <div className="h-10 w-10 bg-[#FDFDFD] flex items-center justify-center rounded-md">
@@ -25,7 +28,7 @@ function Classes() {
     },
     {
       id: 2,
-      subject: "Maths",
+      subject: 'Maths',
       chapters: 21,
       icon: (
         <div className="h-10 w-10 bg-[#FDFDFD] flex items-center justify-center rounded-md">
@@ -35,7 +38,7 @@ function Classes() {
     },
     {
       id: 3,
-      subject: "Physics",
+      subject: 'Physics',
       chapters: 30,
       icon: (
         <div className="h-10 w-10 bg-[#FDFDFD] flex items-center justify-center rounded-md">
@@ -45,7 +48,7 @@ function Classes() {
     },
     {
       id: 4,
-      subject: "Chemistry",
+      subject: 'Chemistry',
       chapters: 8,
       icon: (
         <div className="h-10 w-10 bg-[#FDFDFD] flex items-center justify-center rounded-md">
@@ -55,7 +58,7 @@ function Classes() {
     },
     {
       id: 5,
-      subject: "Organic Chemistry",
+      subject: 'Organic Chemistry',
       chapters: 10,
       icon: (
         <div className="h-10 w-10 bg-[#FDFDFD] flex items-center justify-center rounded-md">
@@ -65,7 +68,7 @@ function Classes() {
     },
     {
       id: 6,
-      subject: "Inorganic Chemistry",
+      subject: 'Inorganic Chemistry',
       chapters: 12,
       icon: (
         <div className="h-10 w-10 bg-[#FDFDFD] flex items-center justify-center rounded-md">
@@ -75,7 +78,7 @@ function Classes() {
     },
     {
       id: 7,
-      subject: "Mathematics Adva..",
+      subject: 'Mathematics Adva..',
       chapters: 14,
       icon: (
         <div className="h-10 w-10 bg-[#FDFDFD] flex items-center justify-center rounded-md">
@@ -86,9 +89,9 @@ function Classes() {
   ];
 
   return (
-    <div className=" font-Poppins min-h-screen py-6 md:full md:mr-8 flex flex-col bg-gray-100/60 ">
+    <div className="font-Poppins min-h-screen py-6 md:full md:mr-8 flex flex-col bg-gray-100/60 ">
       <CourseDetailNavbar courseRoute={params.coursedetail} />
-      <div className="flex flex-col  bg-white rounded-xl p-3 md:mr-10 mt-4 md:ml-10">
+      <div className="flex flex-col bg-white rounded-xl p-3 md:mr-10 mt-4 md:ml-10">
         <div className="m-4">
           <h2 className="text-[20px] font-[550]">Subject</h2>
           <p className="text-[14px] text-gray-500 mt-2">
@@ -99,16 +102,24 @@ function Classes() {
           {subjects.map((ele) => (
             <Link
               href={`/dashboard/my-course/${params.coursedetail}/classes/${ele.subject}`}
-              className="flex gap-3 m-4 border border-gray-100 p-3  pt-5 pb-5 rounded-lg "
+              className="m-4"
               key={ele.id}
             >
-              <div>{ele.icon}</div>
-              <div className="flex flex-col">
-                <h2 className="text-[14px]">{ele.subject}</h2>
-                <p className="text-[12px] text-[#666666] font-medium">
-                  {ele.chapters} Chapters
-                </p>
-              </div>
+              <motion.div
+                whileHover={{
+                  scale: 1.05,
+                  boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.15)',
+                }}
+                className="flex gap-3 border border-gray-100 p-3 pt-5 pb-5 rounded-lg transition duration-300 ease-in-out"
+              >
+                <div>{ele.icon}</div>
+                <div className="flex flex-col">
+                  <h2 className="text-[14px]">{ele.subject}</h2>
+                  <p className="text-[12px] text-[#666666] font-medium">
+                    {ele.chapters} Chapters
+                  </p>
+                </div>
+              </motion.div>
             </Link>
           ))}
         </div>
--- components/Dashboard/Course/Subject.jsx
+++ components/Dashboard/Course/Subject.jsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { motion } from "framer-motion";
 import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
 import React from "react";
 import { IoMdArrowBack } from "react-icons/io";
+
 
 const Subject = ({ subject }) => {
   const router = useRouter();
@@ -94,9 +96,17 @@ const Subject = ({ subject }) => {
                 /\s+/g,
                 "-"
               )}`}
-              className="flex gap-3 m-4 border border-gray-100 p-3  pt-5 pb-5 rounded-lg "
+              className="m-4"
               key={ele.id}
             >
+                <motion.div
+                whileHover={{
+                  scale: 1.05,
+                  boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.15)',
+                }}
+                className="flex gap-3 border border-gray-100 p-3 pt-5 pb-5 rounded-lg transition duration-300 ease-in-out"
+              >
+
               <div className="h-10 w-1 bg-TechBlue rounded-md"></div>
               <div className="flex flex-col">
                 <h2 className="text-[14px] font-semibold">{ele.head}</h2>
@@ -104,6 +114,7 @@ const Subject = ({ subject }) => {
                   {ele.para}
                 </div>
               </div>
+              </motion.div>
             </Link>
           ))}
         </div>
